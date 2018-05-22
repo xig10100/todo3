a76199c6dcfb406cff006076b7b436128602c14b
陈雄 <xig1010@qq.com>
2018-05-22 18:10:31 +0800

    add view

---
+++ app/Http/Controllers/TastController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TastController extends Controller
+{
+    //
+//
+    public function index()
+    {
+        return view('Task.index');
+    }
+}
---
+++ resources/views/Task/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.master')
+
+@section('content')
+    <p>这里是主体内容，完善中...</p>
+@endsection
